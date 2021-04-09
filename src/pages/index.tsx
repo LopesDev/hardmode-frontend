6ff1892ea863f7a07cb657aaf03b62ce158946d6
Matheus Lopes <matlopes1999@gmail.com>
2021-04-08 21:29:13 -0300

    Implementa getServerSideProps e captura o JWT do cookie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 
+import {AuthCookieEnum} from '../constants/AuthSessionConstant';
+
 import { AuthProvider } from '../context/AuthContext';
 import HardModStylesProvider from '../layout/GlobalStyles';
 
@@ -26,6 +28,22 @@ function Home() {
       </HardModStylesProvider>
     </>
   )
+}
+
+export async function getServerSideProps({ req, res, ...context }) {
+  // see the rest of parameters by uncommenting the following line.
+  // console.log(context);
+  const JWT = req.cookies[AuthCookieEnum.CURRENT_COOKIE];
+  if (JWT) {
+    // @Todo Implement get User from JWT;
+    return {
+      props: {}
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 export default Home;
